02ee075dd60280ac7935c08c1653a16a672b66a8
akash <ak.akashpatil5@gmail.com>
2022-03-05 12:58:05 +0530

    click & swap

---
+++ March/Week-1/Assignment-6/index.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 20;
+console.log("======== Veriable befour Swap=======");
+console.log("Veriable a Value: " +a);
+console.log("Veriable b value: " +b);
+a = a + b
+b= a - b
+a =a - b
+
+console.log("======= Veriable after swap =======" );
+console.log("Veriable a Value: " +a);
+console.log("Veriable b value: " +b);
